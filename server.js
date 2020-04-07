9e2cadda109fde7bf7b1de057186ac485f46946e
guruhariharaun <guruhariharaun2000@gmail.com>
2020-04-07 23:55:01 +0530

    port changed

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-let port = 3000;
+let port = process.env.port || 3000;
 app.listen(port, function () {
   console.log('Server is started on http:127.0.0.1:'+port);
 });
