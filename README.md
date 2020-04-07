e76c972080af358af3dc4f85c645a80def31a082
guruhariharaun <guruhariharaun2000@gmail.com>
2020-04-07 23:48:37 +0530

    make it better

---
+++ README.md
@@ -0,0 +1,24 @@
+# Node.js-Register-Login-App
+
+This is a user Registration & Login  app using Node.js, MongoDB.
+
+
+### Prerequisites
+
+Node.js
+NPM
+MongoDB
+
+### Installing
+```
+npm install
+```
+
+### To Run
+```
+nodemon server.js
+```
+
+The server is Running on
++ http://localhost:3000/
+
---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var ejs = require('ejs');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+mongoose.connect('mongodb+srv://gururoxz:guru@testing@cluster0-vatbg.mongodb.net/registrationFormHeruko?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.');
+  } else {
+    console.log('Error in DB connection : ' + err);
+  }
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');	
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+var index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+let port = 3000;
+app.listen(port, function () {
+  console.log('Server is started on http:127.0.0.1:'+port);
+});
