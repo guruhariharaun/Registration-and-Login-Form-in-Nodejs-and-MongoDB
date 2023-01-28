86d4667c8486b030e36888fb60f9209f746363e2
Reeve Fernandes <ferna312@msu.edu>
2023-01-28 18:09:45 -0500

    map added

--- routes/index.js
+++ routes/index.js
@@ -78,6 +78,10 @@ router.post("/login", function (req, res, next) {
   });
 });
 
+router.get("/map", function (req, res, next) {
+  return res.render("map.ejs");
+});
+
 router.get("/profile", function (req, res, next) {
   console.log("profile");
   User.findOne({ unique_id: req.session.userId }, function (err, data) {
