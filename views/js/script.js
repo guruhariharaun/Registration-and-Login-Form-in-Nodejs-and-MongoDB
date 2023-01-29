4c0e197b88f3092db51490fd925de74f0b98efaa
Reeve Fernandes <ferna312@msu.edu>
2023-01-29 06:36:05 -0500

    staging

--- views/js/script.js
+++ views/js/script.js
@@ -1,6 +1,35 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYXJsZWVubW9udGVpcm8iLCJhIjoiY2xkZ2dyYXB2MHhjbDN3cjhwMzVxZHR5aiJ9.nk3cnBcTtUmDjrrWxGeoCA";
 
+const geojson = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.032, 38.913]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Washington, D.C.'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.414, 37.776]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'San Francisco, California'
+      }
+    }
+  ]
+};
+
+
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
   enableHighAccuracy: true,
 });
